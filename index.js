d621c09d8d33ed4bffd9017811fa2b2ea68e4822
gbellini90 <gbellini90@gmail.com>
2018-12-08 21:43:54 -0500

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+// Create a variable named newID that adds 1000000000 to the value in oldID, which you shouldn't define —
+ // it will be provided for you in the test suite.
+
+ const newID = oldID + 1000000000;
+
+ const ageIsValid = Number.isInteger(currentAge);
+
+ const randomNumber = Math.random() *20
+
+ const randomInteger = Math.floor(randomNumber)
+
+ const randomUserID = 1000000000 + randomInteger
